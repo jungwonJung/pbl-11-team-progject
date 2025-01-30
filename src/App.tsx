dd2804929c85f7b4710fa2ad7941345f8e776a4d
Aida <aIsmayilova@socartrading.com>
2025-01-30 01:50:29 +0100

    added new style and responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "./index.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import GlobalStyle from "./styles/GlobalStyles";
