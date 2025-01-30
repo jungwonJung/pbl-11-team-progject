46e097f261e4536187d1328fe11152319204786b
jungwonJung <wjdwjd1501@gmail.com>
2025-01-30 11:51:49 +0100

    hot fix for deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import  { useState } from "react";
 import "./index.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
